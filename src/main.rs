17181c08389556dff5690729d9892a62afe19578
Yog <yog.panjarale@gmail.com>
2021-07-28 23:11:12 +0530

    done

--- src/main.rs
+++ src/main.rs
@@ -3,37 +3,72 @@ extern crate graphics;
 extern crate opengl_graphics;
 extern crate piston;
 
-use piston::window::WindowSettings;
-use piston::event_loop::*;
-use piston::input::*;
+extern crate rand;
+
 use glutin_window::GlutinWindow;
 use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::*;
+use piston::input::*;
+use piston::window::WindowSettings;
 
-#[derive(Clone,PartialEq)]
-enum Direction {
-    Right,Left,Up,Down
-}
-struct Game{
-    gl:GlGraphics,
+use std::collections::LinkedList;
+use std::iter::FromIterator;
+
+pub struct Game {
+    gl: GlGraphics,
+    rows: u32,
+    cols: u32,
     snake: Snake,
+    just_eaten: bool,
+    square_width: u32,
+    food: Food,
+    score: u32,
 }
-impl Game {
-    fn render(&mut self,arg:&RenderArgs){
-        // use graphics;
 
-        let GREEN :[f32;4]= [0.0,1.0,0.0,1.0];
-        self.gl.draw(arg.viewport(), |_c,gl|{
+impl Game {
+    fn render(&mut self, args: &RenderArgs) {
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+
+        self.gl.draw(args.viewport(), |_c, gl| {
             graphics::clear(GREEN, gl);
         });
-        self.snake.render(&mut self.gl, arg);
-        
+
+        self.snake.render(args);
+        self.food.render(&mut self.gl, args, self.square_width);
     }
-    fn update(&mut self){
-        self.snake.update();
+
+    fn update(&mut self, _args: &UpdateArgs) -> bool {
+        if !self.snake.update(self.just_eaten, self.cols, self.rows) {
+            return false;
+        }
+
+        if self.just_eaten {
+            self.score += 1;
+            self.just_eaten = false;
+        }
+
+        self.just_eaten = self.food.update(&self.snake);
+        if self.just_eaten {
+            use rand::thread_rng;
+            use rand::Rng;
+            // try my luck
+            let mut r = thread_rng();
+            loop {
+                let new_x = r.gen_range(0, self.cols);
+                let new_y = r.gen_range(0, self.rows);
+                if !self.snake.is_collide(new_x, new_y) {
+                    self.food = Food { x: new_x, y: new_y };
+                    break;
+                }
+            }
+        }
+
+        true
     }
+
     fn pressed(&mut self, btn: &Button) {
-        let last_direction = self.snake.dir.clone();
-        self.snake.dir = match btn {
+        let last_direction = self.snake.d.clone();
+        self.snake.d = match btn {
             &Button::Keyboard(Key::Up) if last_direction != Direction::Down => Direction::Up,
             &Button::Keyboard(Key::Down) if last_direction != Direction::Up => Direction::Down,
             &Button::Keyboard(Key::Left) if last_direction != Direction::Right => Direction::Left,
@@ -43,56 +78,165 @@ impl Game {
     }
 }
 
-struct Snake{
-    pos_x :i32,
-    pos_y :i32,
-    dir:Direction,
+/// The direction the snake moves in.
+#[derive(Clone, PartialEq)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
 }
+
+pub struct Snake {
+    gl: GlGraphics,
+    snake_parts: LinkedList<SnakePiece>,
+    width: u32,
+    d: Direction,
+}
+
+#[derive(Clone)]
+pub struct SnakePiece(u32, u32);
+
 impl Snake {
-    fn render(&mut self,gl:&mut GlGraphics,arg:&RenderArgs){
-        // use graphics;
-        let RED :[f32;4]= [1.0,0.0,0.0,1.0];
-        let square = graphics::rectangle::square(
-            (self.pos_x *20) as f64, 
-            (self.pos_y *20) as f64, 20_f64);
-        gl.draw(arg.viewport(), |_c,gl|{
-            let transform = _c.transform;
-            graphics::rectangle(RED, square, transform, gl);
+    pub fn render(&mut self, args: &RenderArgs) {
+        const RED: [f32; 4] = [1.0, 0.0, 0.0, 1.0];
+
+        let squares: Vec<graphics::types::Rectangle> = self
+            .snake_parts
+            .iter()
+            .map(|p| SnakePiece(p.0 * self.width, p.1 * self.width))
+            .map(|p| graphics::rectangle::square(p.0 as f64, p.1 as f64, self.width as f64))
+            .collect();
+
+        self.gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            squares
+                .into_iter()
+                .for_each(|square| graphics::rectangle(RED, square, transform, gl));
         })
     }
-    fn update(&mut self){
-        match self.dir{
-            Direction::Right=> self.pos_x += 1,
-            Direction::Left => self.pos_x -= 1,
-            Direction::Up => self.pos_y -= 1,
-            Direction::Down => self.pos_y += 1,
+
+    /// Move the snake if valid, otherwise returns false.
+    pub fn update(&mut self, just_eaten: bool, cols: u32, rows: u32) -> bool {
+        let mut new_front: SnakePiece =
+            (*self.snake_parts.front().expect("No front of snake found.")).clone();
+
+        if (self.d == Direction::Up && new_front.1 == 0)
+            || (self.d == Direction::Left && new_front.0 == 0)
+            || (self.d == Direction::Down && new_front.1 == rows - 1)
+            || (self.d == Direction::Right && new_front.0 == cols - 1)
+        {
+            return false;
         }
+
+        match self.d {
+            Direction::Up => new_front.1 -= 1,
+            Direction::Down => new_front.1 += 1,
+            Direction::Left => new_front.0 -= 1,
+            Direction::Right => new_front.0 += 1,
+        }
+
+        if !just_eaten {
+            self.snake_parts.pop_back();
+        }
+
+        // Checks self collision.
+        if self.is_collide(new_front.0, new_front.1) {
+            return false;
+        }
+
+        self.snake_parts.push_front(new_front);
+        true
+    }
+
+    fn is_collide(&self, x: u32, y: u32) -> bool {
+        self.snake_parts.iter().any(|p| x == p.0 && y == p.1)
     }
 }
+
+pub struct Food {
+    x: u32,
+    y: u32,
+}
+
+impl Food {
+    // Return true if snake ate food this update
+    fn update(&mut self, s: &Snake) -> bool {
+        let front = s.snake_parts.front().unwrap();
+        if front.0 == self.x && front.1 == self.y {
+            true
+        } else {
+            false
+        }
+    }
+
+    fn render(&mut self, gl: &mut GlGraphics, args: &RenderArgs, width: u32) {
+        const BLACK: [f32; 4] = [1.0, 1.0, 1.0, 1.0];
+
+        let x = self.x * width;
+        let y = self.y * width;
+
+        let square = graphics::rectangle::square(x as f64, y as f64, width as f64);
+
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+
+            graphics::rectangle(BLACK, square, transform, gl)
+        });
+    }
+}
+
 fn main() {
-   let opengl =  OpenGL::V3_2;
-   let mut window  :GlutinWindow = WindowSettings::new("My Window",[200,200]).opengl(opengl).exit_on_esc(true).build().unwrap();
+    // Change this to OpenGL::V2_1 if this fails.
+    let opengl = OpenGL::V3_2;
+
+    const COLS: u32 = 30;
+    const ROWS: u32 = 20;
+    const SQUARE_WIDTH: u32 = 20;
+
+    let width = COLS * SQUARE_WIDTH;
+    let height = ROWS * SQUARE_WIDTH;
+
+    let mut window: GlutinWindow = WindowSettings::new("Snake Game", [width, height])
+        .opengl(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
 
     let mut game = Game {
         gl: GlGraphics::new(opengl),
-        snake:Snake{
-            pos_x:0,
-            pos_y:0,
-            dir:Direction::Right,
+        rows: ROWS,
+        cols: COLS,
+        square_width: SQUARE_WIDTH,
+        just_eaten: false,
+        food: Food { x: 1, y: 1 },
+        score: 0,
+        snake: Snake {
+            gl: GlGraphics::new(opengl),
+            snake_parts: LinkedList::from_iter((vec![SnakePiece(COLS / 2, ROWS / 2)]).into_iter()),
+            width: SQUARE_WIDTH,
+            d: Direction::Down,
+        },
+    };
+
+    let mut events = Events::new(EventSettings::new()).ups(4);
+    while let Some(e) = events.next(&mut window) {
+        if let Some(r) = e.render_args() {
+            game.render(&r);
         }
-    }; 
-   let mut events = Events::new(EventSettings::new()).ups(8);
-   while let Some(e) = events.next(&mut window){
-       if let Some(r)= e.render_args(){
-         game.render(&r);
-       }
-       if let Some(e)= e.update_args(){
-           game.update();
-       }
-       if let Some(k) = e.button_args(){
-            if k.state == ButtonState::Press{
+
+        if let Some(u) = e.update_args() {
+            if !game.update(&u) {
+                break;
+            }
+        }
+
+        if let Some(k) = e.button_args() {
+            if k.state == ButtonState::Press {
                 game.pressed(&k.button);
             }
-       }
-   }
+        }
+    }
+    println!("Congratulations, your score was: {}", game.score);
 }
